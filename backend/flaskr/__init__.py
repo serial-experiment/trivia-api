0258bd55a6755e81787a47878b13ae05dffd0dda
Stephen Bennett <stephen.bennett@opencorporates.com>
2023-09-29 22:16:17 +0100

    updates

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -6,7 +6,7 @@ import random
 
 from models import setup_db, Question, Category
 
-QUESTIONS_PER_PAGE = 10
+QUESTIONS_PER_PAGE = 6
 
 def create_app(test_config=None):
     # create and configure the app
@@ -26,12 +26,19 @@ def create_app(test_config=None):
         )
         return response
 
-    @app.route("/categories", methods=["GET"])
+    @app.route('/categories')
     def get_categories():
         categories = Category.query.all()
-        formatted_categories = [category.format() for category in categories]
+        categories_dict = {}
+        for category in categories:
+            categories_dict[category.id] = category.type
+        if (len(categories_dict) == 0):
+            abort(404)
 
-        return jsonify({"success": True, "categories": formatted_categories})
+        return jsonify({
+            'success': True,
+            'categories': categories_dict
+        })
  
     @app.route("/questions", methods=["GET"])
     def get_questions():
@@ -42,14 +49,16 @@ def create_app(test_config=None):
         questions = Question.query.all()
         formatted_questions = [question.format() for question in questions]
         categories = Category.query.all()
-        formatted_categories = [category.format() for category in categories]
-
+        categories_dict = {}
+        for category in categories:
+            categories_dict[category.id] = category.type
+        
         return jsonify(
             {
                 "success": True,
                 "questions": formatted_questions[start:end],
                 "total_questions": len(formatted_questions),
-                "categories": formatted_categories,
+                "categories": categories_dict,
             }
         )
 
@@ -65,49 +74,47 @@ def create_app(test_config=None):
 
         return jsonify({"success": True, "deleted": question_id})
 
-    @app.route("/questions", methods=["POST"])
-    def create_question():
+    @app.route("/questions", methods=['POST'])
+    def add_question():
         body = request.get_json()
 
-        new_question = body.get("question", None)
-        new_answer = body.get("answer", None)
-        new_category = body.get("category", None)
-        new_difficulty = body.get("difficulty", None)
+        if not ('question' in body and 'answer' in body and 'difficulty' in body and 'category' in body):
+            abort(422)
+
+        new_question = body.get('question')
+        new_answer = body.get('answer')
+        new_difficulty = body.get('difficulty')
+        new_category = body.get('category')
 
         try:
-            question = Question(
-                question=new_question,
-                answer=new_answer,
-                category=new_category,
-                difficulty=new_difficulty,
-            )
+            question = Question(question=new_question, answer=new_answer,
+                                difficulty=new_difficulty, category=new_category)
             question.insert()
 
-            return jsonify({"success": True, "created": question.id})
+            return jsonify({
+                'success': True,
+                'created': question.id,
+            })
 
         except:
             abort(422)
 
-    @app.route("/questions/search", methods=["POST"])
+    @app.route('/questions/search', methods=['POST'])
     def search_questions():
         body = request.get_json()
-        search_term = body.get("searchTerm", None)
+        search_term = body.get('searchTerm', None)
 
-        if search_term is None:
-            abort(422)
+        if search_term:
+            search_results = Question.query.filter(
+                Question.question.ilike(f'%{search_term}%')).all()
 
-        questions = Question.query.filter(
-            Question.question.ilike(f"%{search_term}%")
-        ).all()
-        formatted_questions = [question.format() for question in questions]
-
-        return jsonify(
-            {
-                "success": True,
-                "questions": formatted_questions,
-                "total_questions": len(formatted_questions),
-            }
-        )
+            return jsonify({
+                'success': True,
+                'questions': [question.format() for question in search_results],
+                'total_questions': len(search_results),
+                'current_category': None
+            })
+        abort(404)
     
     @app.route("/categories/<int:category_id>/questions", methods=["GET"])
     def get_questions_by_category(category_id):
